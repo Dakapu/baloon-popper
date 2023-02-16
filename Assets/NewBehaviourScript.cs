c55bfdac7d0d4ee615cddc3be4810c528936ab96
Dakapu <24271657+Dakapu@users.noreply.github.com>
2023-02-16 10:13:47 +0100

    add first Script

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int playerHealth = 100;
+        Debug.Log(playerHealth);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
