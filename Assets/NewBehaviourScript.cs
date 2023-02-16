eaaf9698a61ef5bf2af67a5cc613ac3b2b9cc787
Dakapu <24271657+Dakapu@users.noreply.github.com>
2023-02-16 11:23:10 +0100

    script setup

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -7,14 +9,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int playerHealth = 100;
-        Debug.Log(playerHealth);
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0.25f, 0, 0) * Time.deltaTime;       
     }
 }
