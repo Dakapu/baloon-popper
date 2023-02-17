45822a14f753f7c2bd7142feb1187f1fd1e47761
Dakapu <24271657+Dakapu@users.noreply.github.com>
2023-02-17 13:46:54 +0100

    assign random Material on Start

--- Assets/balloonScript.cs
+++ Assets/balloonScript.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class balloonScript : MonoBehaviour
 {
     public int life = 5;
+    public int score = 0;
+    public float scaleToIncrease = 0.5f;
+
+    void Start()
+    {
+        Material redMaterial = Resources.Load("redBalloonMaterial", typeof(Material)) as Material;
+        Material greenMaterial = Resources.Load("greenBalloonMaterial", typeof(Material)) as Material;
+        Material blueMaterial = Resources.Load("blueBalloonMaterial", typeof(Material)) as Material;
+
+        Material[] matArray = new Material[3];
+        matArray[0] = redMaterial;
+        matArray[1] = greenMaterial;
+        matArray[2] = blueMaterial;
+
+        this.GetComponent<Renderer>().material = matArray[Random.Range(0,3)];
+
+    }
+
 
     private void OnMouseDown()
     {
         if (life > 0)
         {
-            transform.localScale += new Vector3(1, 1, 1);
+            transform.localScale += Vector3.one * scaleToIncrease;
             life--;
 
         }
