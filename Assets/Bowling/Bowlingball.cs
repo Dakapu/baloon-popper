126970bca898acbfb4900e5a6ba34f7980986708
Dakapu <24271657+Dakapu@users.noreply.github.com>
2023-03-21 14:07:27 +0100

    setup bowling Minigame
    add script for ball force

---
+++ Assets/Bowling/Bowlingball.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowlingball : MonoBehaviour
+{
+    public float forwardForce;
+    public float leftBorder;
+    public float rightBorder;
+    public float moveIncrement;
+    public Rigidbody rig;
+
+    private void Start()
+    {
+        Bowl();
+    }
+
+    void Bowl()
+    {
+        rig.AddForce(transform.forward * forwardForce);
+
+    }
+
+}
